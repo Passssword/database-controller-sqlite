a6e95bf164e95bb3d165a2b1a4d6224d0af7fedf
Ilya <Stepanenko.my-mail@yandex.ru>
2024-03-19 16:15:40 +1100

    Signed-off-by: Ilya <Stepanenko.my-mail@yandex.ru>

---
+++ server_http.js
@@ -0,0 +1,73 @@
+const writeData = require('./source/dataController.js').writeData;
+const getData = require('./source/dataController.js').getData;
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+	const url = req.url;
+	console.log(url)
+	console.log( req.method )
+
+	switch (url) {
+		case '/':
+			let data = fs.readFileSync( './client/index.html', { encoding: 'utf-8', flag: 'r'} )
+			res.write(data)
+			res.end()
+			break;
+		case '/getdata':
+			let temp_data = getData();
+			res.write(temp_data)
+			res.statusCode = 200;
+			res.end()
+			break;
+		case '/pushdata':
+			if ( req.method == 'POST' ) {
+				let body = '';
+				req.on( 'data', chunk => {
+					body += chunk.toString()
+				} )
+				req.on( 'end', () => {
+					try{
+						let dataObj = JSON.parse(body)
+						writeData(dataObj)
+						res.write('OK')
+						return res.end()
+					}catch(err){
+						console.log(err)
+						res.write('something wrong')
+						return res.end()
+					}
+				} )
+				
+			}
+			break;
+		default:
+			if ( url.includes('/images') ) {
+				let data = fs.readFileSync('./client'+url)
+				res.write(data)
+				res.end()
+			} else if ( url.includes('/styles') ) {
+				let data = fs.readFileSync('./client'+url)
+				res.write(data)
+				res.end()
+			} else if ( url.includes('/js') ) {
+				let data = fs.readFileSync('./client'+url)
+				res.write(data)
+				res.end()
+			} else {
+				res.statusCode = 404;
+				res.write('<h1>404</h1>')
+				res.end()
+			}
+	}
+} )
+
+server.listen(34587, function() {
+	console.log('server working on port: 34587')
+});
